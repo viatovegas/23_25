b8bd4a0ab8ea186f021f9ee8df7167492ce96af3
viatovegas <145833157+viatovegas@users.noreply.github.com>
2023-12-25 14:43:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// JavaScript
 window.onload = function() {
   const spinner = document.getElementById('loading');
   spinner.classList.add('loaded');
@@ -13,63 +12,65 @@ for(let i = 0; i < items.length; i++){
       translate: ['0 50px', 0],
       filter: ['blur(10px)', 'blur(0)'],
     };
-    
+
     const options = {
       duration: 600,
       delay: i * 400,
       fill: 'forwards',
     };
-  
+
     items[i].animate(keyframes, options);
   }
 
-
-(function() {
-  let cc = document.querySelector('.cookie-consent');
-  let ca = document.querySelector('.cookie-agree');
-  const flag = localStorage.getItem('popupFlag');
-  if (flag != null) {
-    const data = JSON.parse(flag);
-    if (data['value'] == 'true') {
-      window.onscroll = () => {
-        if (window.pageYOffset) {
-          popup();
-        }
-      }
-    } else {
-      const current = new Date();
-      if (current.getTime() > data['expire']) {
-        setWithExpiry('popupFlag', 'true', expire);
+  (function() {
+    let cc = document.querySelector('.cookie-consent');
+    let ca = document.querySelector('.cookie-agree');
+    const flag = localStorage.getItem('popupFlag');
+    if (flag != null) {
+      const data = JSON.parse(flag);
+      if (data['value'] == 'true') {
         window.onscroll = () => {
           if (window.pageYOffset) {
             popup();
           }
         }
-      }      
-    }
-  } else {
-    setWithExpiry('popupFlag', 'true', expire);
-    window.onscroll = () => {
-      if (window.pageYOffset) {
-        popup();
+      } else {
+        const current = new Date();
+        if (current.getTime() > data['expire']) {
+          setWithExpiry('popupFlag', 'true', expire);
+          window.onscroll = () => {
+            if (window.pageYOffset) {
+              popup();
+            }
+          }
+        }
+      }
+    } else {
+      setWithExpiry('popupFlag', 'true', expire);
+      window.onscroll = () => {
+        if (window.pageYOffset) {
+          popup();
+        }
       }
     }
-  }
-  ca.addEventListener('click', () => {
-    cc.classList.add('cc-hide1');
-  });
-  
-  function popup() {
-    cc.classList.add('is-show');
-  }
-}());
+    ca.addEventListener('click', () => {
+      cc.classList.add('cc-hide1');
+    });
 
 
-const map = L.map('map').setView([29.393184057355096, -98.48380711591551], 15);
+    function popup() {
+      cc.classList.add('is-show');
+    }
+  }());
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  const map = L.map('map').setView([30.30270071203621, -97.67324649633491], 15);
 
-L.marker([29.393184057355096, -98.48380711591551]).addTo(map)
-  .bindPopup('Cooming with us');
+  // タイルレイヤーを作成し、地図にセットする
+  // Open Street Map
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([30.30270071203621, -97.67324649633491]).addTo(map)
+    .bindPopup('See ya ;)');
+
